de1d3671d6f7ab56cc379546fe51ecd8d7818c96
Zakariaedes <zakariaedes@gmail.com>
2023-02-13 02:21:56 +0000

    Creating match game example

--- FantasyCombat.playground/Contents.swift
+++ FantasyCombat.playground/Contents.swift
@@ -1,1 +1,9 @@
 import Foundation
+
+
+var player1: Player = try! Player(name: "Midoriya", attack: 10, defence: 3, speed: 50, life: 35, luck: 50)
+var player2: Player = try! Player(name: "Bakugo", attack: 10, defence: 5, speed: 40, life: 50, luck: 0)
+let round: Round = try! .init(number: 1, player1: player1, player2: player2)
+let match: Match = .init(round: round)
+
+try? match.play()
